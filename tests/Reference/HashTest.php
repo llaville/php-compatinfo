6d1365d9b40c811b1b048d24a432624a83a1988a
Remi Collet <fedora@famillecollet.com>
2011-04-04 18:30:38 +0200

    fix "hash" tests when "mhash" is emulated (optionnal in php 5.3.0)

--- tests/Reference/HashTest.php
+++ tests/Reference/HashTest.php
@@ -24,6 +24,43 @@ class PHP_CompatInfo_Reference_HashTest extends PHP_CompatInfo_Reference_Generic
      */
     protected function setUp()
     {
+        // Since php 5.3.0 mhash is emulated by hash ext.
+        // So this constants/functions are reported in "hash"
+        $this->ignoredconstants = array(
+            'MHASH_CRC32',
+            'MHASH_MD5',
+            'MHASH_SHA1',
+            'MHASH_HAVAL256',
+            'MHASH_RIPEMD160',
+            'MHASH_TIGER',
+            'MHASH_GOST',
+            'MHASH_CRC32B',
+            'MHASH_HAVAL224',
+            'MHASH_HAVAL192',
+            'MHASH_HAVAL160',
+            'MHASH_HAVAL128',
+            'MHASH_TIGER128',
+            'MHASH_TIGER160',
+            'MHASH_MD4',
+            'MHASH_SHA256',
+            'MHASH_ADLER32',
+            'MHASH_SHA224',
+            'MHASH_SHA512',
+            'MHASH_SHA384',
+            'MHASH_WHIRLPOOL',
+            'MHASH_RIPEMD128',
+            'MHASH_RIPEMD256',
+            'MHASH_RIPEMD320',
+            'MHASH_SNEFRU256',
+            'MHASH_MD2',
+        );
+        $this->ignoredfunctions = array(
+            'mhash',
+            'mhash_count',
+            'mhash_get_block_size',
+            'mhash_get_hash_name',
+            'mhash_keygen_s2k',
+        );
         $this->obj = new PHP_CompatInfo_Reference_Hash();
         parent::setUp();
     }
