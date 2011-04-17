b65986f18482506433fe3035712dd586eb2e5e33
Remi Collet <fedora@famillecollet.com>
2011-04-17 09:01:49 +0200

    improve Ref for mbstring, add function aliases

--- tests/Reference/MbstringTest.php
+++ tests/Reference/MbstringTest.php
@@ -24,23 +24,6 @@ class PHP_CompatInfo_Reference_MbstringTest extends PHP_CompatInfo_Reference_Gen
      */
     protected function setUp()
     {
-        $this->ignoredfunctions = array(
-            // Function aliases
-            'mbregex_encoding',
-            'mbereg',
-            'mberegi',
-            'mbereg_replace',
-            'mberegi_replace',
-            'mbsplit',
-            'mbereg_match',
-            'mbereg_search',
-            'mbereg_search_pos',
-            'mbereg_search_regs',
-            'mbereg_search_init',
-            'mbereg_search_getregs',
-            'mbereg_search_getpos',
-            'mbereg_search_setpos',
-        );
         $this->obj = new PHP_CompatInfo_Reference_Mbstring();
         parent::setUp();
     }
