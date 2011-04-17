a951f305bf1fc1cd8046541c4605f76a0dbdee05
Remi Collet <fedora@famillecollet.com>
2011-04-17 09:11:04 +0200

    improved GenericTest : test classes which should not exists, according to Ref

--- tests/Reference/GenericTest.php
+++ tests/Reference/GenericTest.php
@@ -175,6 +175,13 @@ class PHP_CompatInfo_Reference_GenericTest extends PHPUnit_Framework_TestCase
                     "Class '$constname', found in Reference, doesnt exists."
                 );
             }
+            if (($min && version_compare(PHP_VERSION,$min)<0)
+                || ($max && version_compare(PHP_VERSION,$max)>=0)) {
+                $this->assertFalse(
+                    class_exists($constname, false),
+                    "Class '$constname', found in Reference ($min,$max), exists."
+                );
+            }
         }
     }
 
