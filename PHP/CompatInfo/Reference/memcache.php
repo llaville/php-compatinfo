a49069c43667d56441bca08c7819240d0b96f99a
Remi Collet <fedora@famillecollet.com>
2011-07-03 10:27:13 +0200

    new extension : memcache

---
+++ PHP/CompatInfo/Reference/memcache.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * Version informations about memcache extension
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Remi Collet <Remi@FamilleCollet.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  SVN: $Id$
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ */
+
+/**
+ * All interfaces, classes, functions, constants about memcache extension
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Remi Collet <Remi@FamilleCollet.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: @package_version@
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ * @link     http://www.php.net/manual/en/book.memcache.php
+ */
+class PHP_CompatInfo_Reference_Memcache implements PHP_CompatInfo_Reference
+{
+    /**
+     * Gets all informations at once about:
+     * extensions, interfaces, classes, functions, constants
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getAll($extension = null, $version = null)
+    {
+        $references = array(
+            'extensions' => $this->getExtensions($extension, $version),
+            'interfaces' => $this->getInterfaces($extension, $version),
+            'classes'    => $this->getClasses($extension, $version),
+            'functions'  => $this->getFunctions($extension, $version),
+            'constants'  => $this->getConstants($extension, $version),
+        );
+        return $references;
+    }
+
+    /**
+     * Gets informations about extensions
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getExtensions($extension = null, $version = null)
+    {
+        $extensions = array(
+            'memcache' => array('4.3.0', '', '3.0.6')
+        );
+        return $extensions;
+    }
+
+    /**
+     * Gets informations about interfaces
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getInterfaces($extension = null, $version = null)
+    {
+        $interfaces = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+            );
+            $interfaces = array_merge(
+                $interfaces,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $interfaces = array_merge(
+                $interfaces,
+                $version5
+            );
+        }
+        return $interfaces;
+    }
+
+    /**
+     * Gets informations about classes
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getClasses($extension = null, $version = null)
+    {
+        $classes = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+                'Memcache'                      => array('4.3.3', ''),
+            );
+            $classes = array_merge(
+                $classes,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $classes = array_merge(
+                $classes,
+                $version5
+            );
+        }
+
+        return $classes;
+    }
+
+    /**
+     * Gets informations about functions
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     * @link   http://www.php.net/manual/en/ref.memcache.php
+     */
+    public function getFunctions($extension = null, $version = null)
+    {
+        $functions = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+                // version 0.2.0
+                'memcache_add'                      => array('4.3.3', ''),
+                'memcache_connect'                  => array('4.3.3', ''),
+                'memcache_debug'                    => array('4.3.3', ''),
+                'memcache_decrement'                => array('4.3.3', ''),
+                'memcache_delete'                   => array('4.3.3', ''),
+                'memcache_get'                      => array('4.3.3', ''),
+                'memcache_get_stats'                => array('4.3.3', ''),
+                'memcache_get_version'              => array('4.3.3', ''),
+                'memcache_increment'                => array('4.3.3', ''),
+                'memcache_replace'                  => array('4.3.3', ''),
+                'memcache_set'                      => array('4.3.3', ''),
+                // version 0.4.0
+                'memcache_close'                    => array('4.3.3', ''),
+                'memcache_pconnect'                 => array('4.3.3', ''),
+                // version 1.0.0
+                'memcache_flush'                    => array('4.3.3', ''),
+                // version 2.0.0
+                'memcache_add_server'               => array('4.3.3', ''),
+                'memcache_get_extended_stats'       => array('4.3.3', ''),
+                'memcache_set_compress_threshold'   => array('4.3.3', ''),
+                // version 2.1.0
+                'memcache_get_server_status'        => array('4.3.3', ''),
+                'memcache_set_server_params'        => array('4.3.3', ''),
+                // version 3.0.0
+                'memcache_append'                   => array('4.3.3', ''),
+                'memcache_cas'                      => array('4.3.3', ''),
+                'memcache_prepend'                  => array('4.3.3', ''),
+                'memcache_set_failure_callback'     => array('4.3.3', ''),
+            );
+            $functions = array_merge(
+                $functions,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $functions = array_merge(
+                $functions,
+                $version5
+            );
+        }
+        return $functions;
+    }
+
+    /**
+     * Gets informations about constants
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     * @link   http://www.php.net/manual/en/memcache.constants.php
+     */
+    public function getConstants($extension = null, $version = null)
+    {
+        $constants = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+                'MEMCACHE_COMPRESSED'               => array('4.3.3', ''),
+                'MEMCACHE_HAVE_SESSION'             => array('4.3.3', ''),
+            );
+            $constants = array_merge(
+                $constants,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $constants = array_merge(
+                $constants,
+                $version5
+            );
+        }
+
+        return $constants;
+    }
+
+}
