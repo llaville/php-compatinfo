5ecec98a6122bf51985142600a39c03d12e28dfe
Laurent Laville <pear@laurent-laville.org>
2012-02-24 10:57:24 +0100

    Unified package script generator

--- package.php
+++ package.php
@@ -1,143 +1,338 @@
 <?php
+/**
+ * Unified package script generator.
+ *
+ * Build PEAR package easily, quickly.
+ * Its own package.ini file looks simple enough to edit and maintain.
+ *
+ * Credits to https://github.com/c9s/Onion
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  SVN: $Id:$
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ */
+
 require_once 'PEAR/PackageFileManager2.php';
+
+/**
+ * Generates installexceptions, or exceptions key pfm option
+ *
+ * @param mixed $rules File mapping role exception
+ *
+ * @return array
+ */
+function mappingExceptions($rules)
+{
+    $exceptions = array();
+    foreach ($rules as $exception) {
+        $rule = explode(',', $exception);
+
+        if (count($rule)) {
+            list($file, $role) = array_map('trim', $rule);
+            $exceptions[$file] = $role;
+        }
+    }
+    return $exceptions;
+}
+
+/**
+ * Build list of a category of maintainer
+ *
+ * @param array  $authors Maintainers list
+ * @param string $type    Maintainer category (lead, helper, contributor)
+ * @param object $pfm     Instance of PEAR_PackageFileManager2
+ *
+ * @return void
+ */
+function addMaintainer($authors, $type, $pfm)
+{
+    // author info: {name}, {userid} [, {email} [, {inactive}]]
+    foreach ($authors as $author) {
+        $matches = explode(',', $author);
+        if (count($matches) > 1) {
+            $matches = array_map('trim', $matches);
+            $active  = 'yes';
+            if (isset($matches[3])) {
+                $active = ($matches[3] === 'inactive') ? 'no' : $active;
+            }
+            $email = (isset($matches[2])) ? $matches[2] : '';
+            $pfm->addMaintainer($type, $matches[1], $matches[0], $email, $active);
+        }
+    }
+}
+
+/**
+ * Build list of packages and extensions dependencies
+ *
+ * @param array  $dependencies List of packages and extensions dependencies
+ * @param string $type         'required' or 'optional' dependencies
+ * @param object $pfm          Instance of PEAR_PackageFileManager2
+ *
+ * @return void
+ */
+function addDep($dependencies, $type, $pfm)
+{
+    $deps = array_keys($dependencies);
+
+    foreach ($deps as $dep) {
+        if (preg_match('/ext\/(.*)/', $dep, $matches)) {
+            $pfm->addExtensionDep($type, $matches[1]);
+
+        } elseif (preg_match('/(.*)\/(.*)/', $dep, $matches)) {
+            $pfm->addPackageDepWithChannel(
+                $type, $matches[2], $matches[1], $dependencies[$dep]
+            );
+        }
+    }
+}
+
+/**
+ * Adds OS EOL task on a files list
+ *
+ * @param mixed  $files List of file to apply OS EOL condition
+ * @param string $os    Identify OS (windows or unix)
+ * @param object $pfm   Instance of PEAR_PackageFileManager2
+ *
+ * @return void
+ */
+function addEol($files, $os, $pfm)
+{
+    if (!is_array($files)) {
+        $files = array($files);
+    }
+    foreach ($files as $file) {
+        if ('windows' == $os) {
+            $pfm->addWindowsEol($file);
+        } else {
+            $pfm->addUnixEol($file);
+        }
+    }
+}
+
+/**
+ * Adds install OS conditions
+ *
+ * @param mixed  $rules List of conditions to apply
+ * @param string $os    Identify OS (windows or unix)
+ * @param object $pfm   Instance of PEAR_PackageFileManager2
+ *
+ * @return void
+ */
+function addInstallCondition($rules, $os, $pfm)
+{
+    $pfm->addRelease();
+    if ('windows' == $os) {
+        $pfm->setOSInstallCondition('windows');
+    }
+
+    if (!is_array($rules)) {
+        $rules = array($rules);
+    }
+    foreach ($rules as $rule) {
+        $rule = explode(',', $rule);
+
+        if (count($rule)) {
+            list($path, $as) = array_map('trim', $rule);
+            $pfm->addInstallAs($path, $as);
+        }
+    }
+}
+
+/**
+ * Adds ignores OS conditions
+ *
+ * @param mixed  $rules List of conditions to apply
+ * @param object $pfm   Instance of PEAR_PackageFileManager2
+ *
+ * @return void
+ */
+function addIgnoreCondition($rules, $pfm)
+{
+    if (!is_array($rules)) {
+        $rules = array($rules);
+    }
+    foreach ($rules as $rule) {
+        $rule = explode(',', $rule);
+
+        if (count($rule)) {
+            list($path, $as) = array_map('trim', $rule);
+            $pfm->addIgnoreToRelease($path, $as);
+        }
+    }
+
+}
+
+$filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'package.ini';
+if (!file_exists($filename)) {
+    echo 'Configuration file "package.ini" does not exist';
+    exit(1);
+}
+
+$ini = parse_ini_file($filename, true);
+
+if ($ini === false) {
+    echo 'Cannot parse configuration file "package.ini"';
+    exit(1);
+}
 
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$p2 = new PEAR_PackageFileManager2();
+$pfm = new PEAR_PackageFileManager2();
 
-$name        = 'PHP_CompatInfo';
-$summary     = 'Find out the minimum version and the extensions required for a piece of code to run';
-$description = 'PHP_CompatInfo will parse a file/folder/array to find out the minimum
-version and extensions required for it to run. CLI version has many reports
-(extension, interface, class, function, constant) to display and ability to show
-content of dictionary references.
-';
-//$channel     = 'pear.php.net';
-$channel     = 'bartlett.laurent-laville.org';
+// files to ignore
+if (isset($ini['options']['ignores'])) {
+    $ignores = (array)$ini['options']['ignores'];
+} else {
+    $ignores = array();
+}
+$ignores[] = basename(__FILE__);
 
-$release_state   = 'stable';
-//$release_version = '2.2.0snapshot' . date('Ymd');
-$release_version = '2.2.5';
+// files mapping install exceptions
+$installexceptions = array();
+if (isset($ini['options']['installexceptions'])) {
+    $installexceptions = mappingExceptions($ini['options']['installexceptions']);
+}
 
-$api_state       = 'stable';
-$api_version     = '2.2.0';
-$release_notes   = "
-Additions and changes:
-- version 2.2.5 or better will support all PHP 5.4.0RC,
-  and future version 2.3.0 is reserved for final PHP 5.4.0 stable release
-- add support of new PHP 5.4 classes :
-  SPL: CallbackFilterIterator, RecursiveCallbackFilterIterator
-  Intl: Transliterator, Spoofchecker
-  Session: SessionHandler
-- add support of new PHP 5.4 interfaces :
-  Json: JsonSerializable
-  Session: SessionHandlerInterface
-- add 1 new references data dictionnaries related to extensions: Reflection,
-- add support for Growl test listener ( https://github.com/llaville/phpunit-GrowlTestListener )
+// files mapping exceptions
+$exceptions = array();
+if (isset($ini['options']['exceptions'])) {
+    $exceptions = mappingExceptions($ini['options']['exceptions']);
+}
 
-Bug fixes:
-- none
-";
-$license = array('BSD License', 'http://www.opensource.org/licenses/bsd-license.php');
-
-$p2->setOptions(
-    array(
-        'packagedirectory'  => dirname(__FILE__),
-        'baseinstalldir'    => 'Bartlett',
-        'filelistgenerator' => 'file',
-        'simpleoutput'      => true,
-        'clearcontents'     => false,
-        'changelogoldtonew' => false,
-        'ignore'            => array(
-            basename(__FILE__),
-            '.git', '*.log',
-            'Thumbs.db', 'packageBeta*.xml', 'packageRC*.xml',
-            'HOWTO.txt', 'genext.php',
-            'PEAR.php', 'netgrowl.php',
-            'IssueTest2.php',
-            'PackageTest.php',
-            'source3657.php',
-            'source7813.php',
-            'source13873.php',
-            ),
-        'installexceptions' => array(
-            'phpcompatinfo.xml.dist' => '',
-            'scripts/phpci' => '',
-            'scripts/phpci.bat' => '',
-        ),
-        'exceptions'        => array(
-            'LICENSE' => 'doc',
-            'phpunit.xml' => 'test',
-            'phpcompatinfo.xml.dist' => 'cfg',
-            'README.markdown' => 'doc',
-            ),
-    )
+$options = array(
+    'packagedirectory'  => dirname(__FILE__),
+    'baseinstalldir'    => $ini['options']['baseinstalldir'],
+    'filelistgenerator' => $ini['options']['filelistgenerator'],
+    'simpleoutput'      => $ini['options']['simpleoutput'],
+    'clearcontents'     => $ini['options']['clearcontents'],
+    'changelogoldtonew' => $ini['options']['changelogoldtonew'],
+    'ignore'            => $ignores,
+    'installexceptions' => $installexceptions,
+    'exceptions'        => $exceptions,
 );
+$pfm->setOptions($options);
 
-$p2->setPackage($name);
-$p2->setChannel($channel);
-//$p2->setUri($uri);
-$p2->setSummary($summary);
-$p2->setDescription($description);
+$pfm->setPackage($ini['package']['name']);
+$pfm->setChannel($ini['package']['channel']);
+$pfm->setSummary($ini['package']['summary']);
+$pfm->setDescription($ini['package']['desc']);
 
-$p2->setPackageType('php');
-$p2->setReleaseVersion($release_version);
-$p2->setReleaseStability($release_state);
-$p2->setAPIVersion($api_version);
-$p2->setAPIStability($api_state);
-$p2->setNotes($release_notes);
-$p2->setLicense($license[0], $license[1]);
+$pfm->setPackageType('php');
+$pfm->setReleaseVersion($ini['package']['version']);
+$pfm->setReleaseStability($ini['package']['stability.release']);
+$pfm->setAPIVersion($ini['package']['version.api']);
+$pfm->setAPIStability($ini['package']['stability.api']);
+$pfm->setNotes($ini['package']['notes']);
 
-$p2->setPhpDep('5.2.0');
-$p2->setPearinstallerDep('1.9.0');
+// default license
+$license = array('PHP', false);
 
-$p2->addPackageDepWithChannel('required',
-                              'Base', 'components.ez.no', '1.8');
-$p2->addPackageDepWithChannel('required',
-                              'ConsoleTools', 'components.ez.no', '1.6.1');
-$p2->addPackageDepWithChannel('required',
-                              'Console_CommandLine', 'pear.php.net', '1.1.3');
-$p2->addPackageDepWithChannel('required',
-                              'PHP_Reflect', 'bartlett.laurent-laville.org', '1.3.0');
-$p2->addPackageDepWithChannel('required',
-                              'PHP_Timer', 'pear.phpunit.de', '1.0.0');
+if (isset($ini['package']['license'])) {
+    // license info: {license} [, {uri}]
+    $matches = explode(',', $ini['package']['license']);
+    $match   = count($matches);
+    if ($match > 0) {
+        $matches = array_map('trim', $matches);
+        if ($match == 1) {
+            // without URI
+            $license = array($matches[0], false);
+        } else {
+            // with URI
+            $license = array($matches[0], $matches[1]);
+        }
+    }
+}
+$pfm->setLicense($license[0], $license[1]);
 
-$p2->addPackageDepWithChannel('optional',
-                              'Net_Growl', 'pear.php.net', '2.4.0');
-$p2->addPackageDepWithChannel('optional',
-                              'PHPUnit', 'pear.phpunit.de', '3.5.0');
+$pfm->setPhpDep($ini['require']['php']);
+$pfm->setPearinstallerDep($ini['require']['pearinstaller']);
 
-$p2->addExtensionDep('required', 'dom');
-$p2->addExtensionDep('required', 'libxml');
-$p2->addExtensionDep('required', 'pcre');
-$p2->addExtensionDep('required', 'spl');
+// required dependencies
+if (isset($ini['require'])) {
+    addDep($ini['require'], 'required', $pfm);
+}
 
-$p2->addMaintainer('lead', 'farell', 'Laurent Laville', 'pear@laurent-laville.org');
-$p2->addMaintainer('contributor', 'remicollet', 'Remi Collet', '');
+// optional dependencies
+if (isset($ini['optional'])) {
+    addDep($ini['optional'], 'optional', $pfm);
+}
 
-$p2->addGlobalReplacement('package-info', '@package_version@', 'version');
+// lead
+addMaintainer($ini['package']['authors'], 'lead', $pfm);
 
-$p2->addReplacement('PHP/CompatInfo/CLI.php', 'pear-config', '@cfg_dir@', 'cfg_dir');
-$p2->addReplacement('scripts/phpci', 'pear-config', '@php_bin@', 'php_bin');
-$p2->addReplacement('scripts/phpci.bat', 'pear-config', '@php_bin@', 'php_bin');
-$p2->addReplacement('scripts/phpci.bat', 'pear-config', '@bin_dir@', 'bin_dir');
+// helper
+if (isset($ini['package']['helpers'])) {
+    addMaintainer($ini['package']['helpers'], 'helper', $pfm);
+}
 
-$p2->addWindowsEol('scripts/phpci.bat');
-$p2->addUnixEol('scripts/phpci');
+// contributor
+if (isset($ini['package']['contributors'])) {
+    addMaintainer($ini['package']['contributors'], 'contributor', $pfm);
+}
 
-$p2->addRelease();
-$p2->setOSInstallCondition('windows');
-$p2->addInstallAs('scripts/phpci.bat', 'phpci.bat');
-$p2->addInstallAs('scripts/phpci', 'phpci');
-$p2->addRelease();
-$p2->addInstallAs('scripts/phpci', 'phpci');
-$p2->addIgnoreToRelease('scripts/phpci.bat', 'phpci.bat');
+// replaces
+if (isset($ini['replacements'])) {
+    foreach ($ini['replacements'] as $file => $rules) {
+        if (!is_array($rules)) {
+            $rules = array($rules);
+        }
+        foreach ($rules as $rule) {
+            $rule = explode(',', $rule);
 
-$p2->generateContents();
+            if (count($rule)) {
+                list($type, $from, $to) = array_map('trim', $rule);
+                if ($file === '*') {
+                    $pfm->addGlobalReplacement($type, $from, $to);
+                } else {
+                    $pfm->addReplacement($file, $type, $from, $to);
+                }
+            }
+        }
+    }
+}
+
+// EOL
+if (isset($ini['installConditions']['windowsEol'])) {
+    addEol($ini['installConditions']['windowsEol'], 'windows', $pfm);
+}
+if (isset($ini['installConditions']['unixEol'])) {
+    addEol($ini['installConditions']['unixEol'], 'unix', $pfm);
+}
+
+// OS install conditions
+if (isset($ini['installConditions']['windows.installAs'])) {
+    addInstallCondition(
+        $ini['installConditions']['windows.installAs'], 'windows', $pfm
+    );
+}
+if (isset($ini['installConditions']['windows.ignores'])) {
+    addIgnoreCondition(
+        $ini['installConditions']['windows.ignores'], $pfm
+    );
+}
+if (isset($ini['installConditions']['other.installAs'])) {
+    addInstallCondition(
+        $ini['installConditions']['other.installAs'], 'other', $pfm
+    );
+}
+if (isset($ini['installConditions']['other.ignores'])) {
+    addIgnoreCondition(
+        $ini['installConditions']['other.ignores'], $pfm
+    );
+}
+
+// generates XML
+$pfm->generateContents();
 
 if (isset($_GET['make'])
-    || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
-    $p2->writePackageFile();
+    || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')
+) {
+    $pfm->writePackageFile();
 } else {
-    $p2->debugPackageFile();
+    $pfm->debugPackageFile();
 }
