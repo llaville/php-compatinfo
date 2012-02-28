fe8a85caa8736e3e2b45a20057b436fafa5d1685
Laurent Laville <pear@laurent-laville.org>
2012-02-28 17:31:40 +0100

    first snapshot of final release 2.3.0 includes Zip extension support

---
+++ PHP/CompatInfo/Reference/zip.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * Version informations about zip extension
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  SVN: $Id$
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ */
+
+/**
+ * All interfaces, classes, functions, constants about zip extension
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: @package_version@
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ * @link     http://www.php.net/manual/en/book.zip.php
+ */
+class PHP_CompatInfo_Reference_Zip implements PHP_CompatInfo_Reference
+{
+    /**
+     * Gets all informations at once about:
+     * extensions, interfaces, classes, functions, constants
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getAll($extension = null, $version = null)
+    {
+        $references = array(
+            'extensions' => $this->getExtensions($extension, $version),
+            'interfaces' => $this->getInterfaces($extension, $version),
+            'classes'    => $this->getClasses($extension, $version),
+            'functions'  => $this->getFunctions($extension, $version),
+            'constants'  => $this->getConstants($extension, $version),
+        );
+        return $references;
+    }
+
+    /**
+     * Gets informations about extensions
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getExtensions($extension = null, $version = null)
+    {
+        $extensions = array(
+            'zip' => array('4.1.0', '', '1.9.1')
+        );
+        return $extensions;
+    }
+
+    /**
+     * Gets informations about interfaces
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getInterfaces($extension = null, $version = null)
+    {
+        $interfaces = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+            );
+            $interfaces = array_merge(
+                $interfaces,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $interfaces = array_merge(
+                $interfaces,
+                $version5
+            );
+        }
+        return $interfaces;
+    }
+
+    /**
+     * Gets informations about classes
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     */
+    public function getClasses($extension = null, $version = null)
+    {
+        $classes = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+            );
+            $classes = array_merge(
+                $classes,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+                'ZipArchive'                     => array('5.2.0', ''),
+            );
+            $classes = array_merge(
+                $classes,
+                $version5
+            );
+        }
+
+        return $classes;
+    }
+
+    /**
+     * Gets informations about functions
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     * @link   http://www.php.net/manual/en/ref.zip.php
+     */
+    public function getFunctions($extension = null, $version = null)
+    {
+        $functions = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+                'zip_close'                      => array('4.1.0', ''),
+                'zip_entry_close'                => array('4.1.0', ''),
+                'zip_entry_compressedsize'       => array('4.1.0', ''),
+                'zip_entry_compressionmethod'    => array('4.1.0', ''),
+                'zip_entry_filesize'             => array('4.1.0', ''),
+                'zip_entry_name'                 => array('4.1.0', ''),
+                'zip_entry_open'                 => array('4.1.0', ''),
+                'zip_entry_read'                 => array('4.1.0', ''),
+                'zip_open'                       => array('4.1.0', ''),
+                'zip_read'                       => array('4.1.0', ''),
+            );
+            $functions = array_merge(
+                $functions,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $functions = array_merge(
+                $functions,
+                $version5
+            );
+        }
+        return $functions;
+    }
+
+    /**
+     * Gets informations about constants
+     *
+     * @param string $extension OPTIONAL
+     * @param string $version   OPTIONAL PHP version
+     *                          (4 => only PHP4, 5 or null => PHP4 + PHP5)
+     *
+     * @return array
+     * @link   http://www.php.net/manual/en/zip.constants.php
+     */
+    public function getConstants($extension = null, $version = null)
+    {
+        $constants = array();
+
+        if ((null == $version ) || ('4' == $version)) {
+            $version4 = array(
+            );
+            $constants = array_merge(
+                $constants,
+                $version4
+            );
+        }
+        if ((null == $version ) || ('5' == $version)) {
+            $version5 = array(
+            );
+            $constants = array_merge(
+                $constants,
+                $version5
+            );
+        }
+
+        return $constants;
+    }
+
+}
---
+++ tests/Reference/ZipTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Unit tests for PHP_CompatInfo package, Zip Reference
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ * @since      Class available since Release 2.3.0
+ */
+
+require_once 'GenericTest.php';
+
+/**
+ * Tests for the PHP_CompatInfo class, retrieving components informations
+ * about Zip extension
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ */
+class PHP_CompatInfo_Reference_ZipTest
+    extends PHP_CompatInfo_Reference_GenericTest
+{
+    /**
+     * Sets up the fixture.
+     *
+     * @covers PHP_CompatInfo_Reference_Zip::getExtensions
+     * @covers PHP_CompatInfo_Reference_Zip::getConstants
+     * @covers PHP_CompatInfo_Reference_Zip::getFunctions
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->obj = new PHP_CompatInfo_Reference_Zip();
+        parent::setUp();
+    }
+}
