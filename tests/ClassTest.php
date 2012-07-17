a060c16959607cb7c72af1a05510472cfe2b560c
Laurent Laville <pear@laurent-laville.org>
2012-07-17 08:55:49 +0200

    without class definition, fix the base version of Baz class

--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -114,7 +114,7 @@ class PHP_CompatInfo_ClassTest extends PHPUnit_Framework_TestCase
                     'excluded' => false,
                 ),
                 'Baz' => array(
-                    'versions' => array('5.0.0', ''),
+                    'versions' => array('4.0.0', ''),
                     'uses' => 1,
                     'sources' => array(TEST_FILES_PATH . 'source1.php'),
                     'excluded' => false,
