96227645f28a9f9943cd7246fd810c8ceb1a190e
Laurent Laville <laurent.laville@gmail.com>
2012-10-03 08:57:41 +0200

    fixed version of since phpdoc tag

--- tests/Reference/IncluedTest.php
+++ tests/Reference/IncluedTest.php
@@ -11,7 +11,7 @@
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version    SVN: $Id$
  * @link       http://php5.laurent-laville.org/compatinfo/
- * @since      Class available since Release 2.8.0beta3
+ * @since      Class available since Release 2.8.0
  */
 
 require_once 'GenericTest.php';
