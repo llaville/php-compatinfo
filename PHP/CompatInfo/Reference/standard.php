db866f419442080c232b79ca4f74495e69562568
Laurent Laville <laurent.laville@gmail.com>
2012-11-26 16:01:04 +0100

    PHP 5.4.9 is already out

--- PHP/CompatInfo/Reference/standard.php
+++ PHP/CompatInfo/Reference/standard.php
@@ -33,7 +33,7 @@ class PHP_CompatInfo_Reference_Standard
     /**
      * Latest version of Extension/Reference supported
      */
-    const REF_VERSION = '5.4.9-dev';
+    const REF_VERSION = '5.4.9';
 
     /**
      * Gets informations about extensions
