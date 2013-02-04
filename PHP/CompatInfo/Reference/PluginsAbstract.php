9d50eadbecda6f5aa6fd1ae4e5a4433f2f02f9ed
Laurent Laville <laurent.laville@gmail.com>
2013-02-04 15:48:39 +0100

    introduce LATEST_PHP_5_5 constant for PHP-5.5 branch

--- PHP/CompatInfo/Reference/PluginsAbstract.php
+++ PHP/CompatInfo/Reference/PluginsAbstract.php
@@ -26,11 +26,12 @@ abstract class PHP_CompatInfo_Reference_PluginsAbstract
     implements PHP_CompatInfo_Reference
 {
     /**
-     * Define latest versions of PHP branches (5.2, 5.3, 5.4, ...)
+     * Define latest versions of PHP branches (5.2, 5.3, 5.4, 5.5)
      */
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.22RC1';
     const LATEST_PHP_5_4 = '5.4.12RC1';
+    const LATEST_PHP_5_5 = '5.5.0-dev';
 
     /**
      * Warning messages generated during loading of extensions references
