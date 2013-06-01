e1117b1a2c1c1cd4900dd91f75d173fc9db08297
Laurent Laville <laurent.laville@gmail.com>
2013-06-01 12:42:40 +0200

    add shebang on phar version

--- build/pharStub.php
+++ build/pharStub.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 $alias = 'phpcompatinfo.phar';
 Phar::mapPhar($alias);
@@ -10,32 +11,12 @@ set_include_path(
     get_include_path()
 );
 
-spl_autoload_register(function ($className) {
-    $classFile = str_replace('_', '/', $className) . '.php';    
-    $excludes  = array(
-        'PHP_CompatInfo_Reference_PluginsAbstract', 
-        'PHP_CompatInfo_Reference_PHP4', 
-        'PHP_CompatInfo_Reference_PHP5', 
-        'PHP_CompatInfo_Reference_ALL'
-    );
-    if (!in_array($className, $excludes)) {
-        $classFile = preg_replace_callback(
-            '#(.*/Reference/)(.*)$#',
-            create_function(
-                '$matches',
-                'return ($matches[1] . strtolower($matches[2]));'
-            ),
-            $classFile
-        );
-    }
-    $classPath = stream_resolve_include_path($classFile);    
-    if ($classPath === FALSE) {
-        return FALSE;
-    }
-    require $classPath;
-});
+require_once 'PHP/Reflect/Autoload.php';
+require_once 'PHP/CompatInfo/Autoload.php';
+require_once 'PEAR/Exception.php';
+require_once 'PHP/Timer/Autoload.php';
 
-define('BARTLETT_COMPOSER_INSTALL', '');
+//define('BARTLETT_COMPOSER_INSTALL', '');
 
 PHP_CompatInfo_CLI::main();
 
