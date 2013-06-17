8a2652db4f6c62e47bc86073c1fcf4e91c5ec425
Laurent Laville <laurent.laville@gmail.com>
2013-06-17 19:41:17 +0200

    update xcache reference to 3.0.2

--- PHP/CompatInfo/Reference/xcache.php
+++ PHP/CompatInfo/Reference/xcache.php
@@ -35,7 +35,7 @@ class PHP_CompatInfo_Reference_XCache
     /**
      * Latest version of Extension/Reference supported
      */
-    const REF_VERSION = '3.0.1';
+    const REF_VERSION = '3.0.2';
 
     /**
      * Gets informations about extensions
