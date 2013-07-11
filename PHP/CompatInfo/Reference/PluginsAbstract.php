4211da522d31df0b3a9f49cf536b58b303957b96
Laurent Laville <laurent.laville@gmail.com>
2013-07-11 21:33:26 +0200

    new PHP versions 5.3.27 and 5.4.17

--- PHP/CompatInfo/Reference/PluginsAbstract.php
+++ PHP/CompatInfo/Reference/PluginsAbstract.php
@@ -29,8 +29,8 @@ abstract class PHP_CompatInfo_Reference_PluginsAbstract
      * Define latest versions of PHP branches (5.2, 5.3, 5.4, 5.5)
      */
     const LATEST_PHP_5_2 = '5.2.17';
-    const LATEST_PHP_5_3 = '5.3.27RC1';
-    const LATEST_PHP_5_4 = '5.4.17RC1';
+    const LATEST_PHP_5_3 = '5.3.27';
+    const LATEST_PHP_5_4 = '5.4.17';
     const LATEST_PHP_5_5 = '5.5.0';
 
     /**
