b124d49d0c765557331e7c23e05c8485bb5d2ba0
Laurent Laville <laurent.laville@gmail.com>
2013-07-22 20:28:27 +0200

    update mongo reference

--- PHP/CompatInfo/Reference/mongo.php
+++ PHP/CompatInfo/Reference/mongo.php
@@ -34,7 +34,7 @@ class PHP_CompatInfo_Reference_Mongo
     /**
      * Latest version of Extension/Reference supported
      */
-    const REF_VERSION = '1.4.1';
+    const REF_VERSION = '1.4.2';
 
     /**
      * Gets informations about extensions
