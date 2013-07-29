3e9193a1c1caebd99fb09c1b6738e9bd0010290f
Laurent Laville <laurent.laville@gmail.com>
2013-07-29 20:14:06 +0200

    Version 2.21 will support PHP 5.4.18

--- PHP/CompatInfo/Reference/PluginsAbstract.php
+++ PHP/CompatInfo/Reference/PluginsAbstract.php
@@ -30,7 +30,7 @@ abstract class PHP_CompatInfo_Reference_PluginsAbstract
      */
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.27';
-    const LATEST_PHP_5_4 = '5.4.17';
+    const LATEST_PHP_5_4 = '5.4.18RC1';
     const LATEST_PHP_5_5 = '5.5.1';
 
     /**
