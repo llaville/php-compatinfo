5ab5852e1a11a48e63c9d6df45bc8e75858728cd
Laurent Laville <laurent.laville@gmail.com>
2013-08-17 10:17:36 +0200

    New stable releases of PHP 5.4 and 5.5 are available

--- PHP/CompatInfo/Reference/PluginsAbstract.php
+++ PHP/CompatInfo/Reference/PluginsAbstract.php
@@ -30,8 +30,8 @@ abstract class PHP_CompatInfo_Reference_PluginsAbstract
      */
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.27';
-    const LATEST_PHP_5_4 = '5.4.18RC2';
-    const LATEST_PHP_5_5 = '5.5.2RC1';
+    const LATEST_PHP_5_4 = '5.4.18';
+    const LATEST_PHP_5_5 = '5.5.2';
 
     /**
      * Warning messages generated during loading of extensions references
