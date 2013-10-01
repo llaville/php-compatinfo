de8f885b0931c6c56f735e24e2668f96c6372c6f
Laurent Laville <laurent.laville@gmail.com>
2013-10-01 08:52:36 +0200

    update xhprof reference to 0.9.4

--- PHP/CompatInfo/Reference/xhprof.php
+++ PHP/CompatInfo/Reference/xhprof.php
@@ -37,7 +37,7 @@ class PHP_CompatInfo_Reference_Xhprof
     /**
      * Latest version of Extension/Reference supported
      */
-    const REF_VERSION = '0.9.3';
+    const REF_VERSION = '0.9.4';
 
     /**
      * Gets informations about extensions
