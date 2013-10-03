9cbd0e0084d17819f4fa735e30d4f6988c7eddad
Laurent Laville <laurent.laville@gmail.com>
2013-10-03 09:37:33 +0200

    first release candidate of PHP 5.4.21 and 5.5.5 are available

--- PHP/CompatInfo/Reference/PluginsAbstract.php
+++ PHP/CompatInfo/Reference/PluginsAbstract.php
@@ -30,8 +30,8 @@ abstract class PHP_CompatInfo_Reference_PluginsAbstract
      */
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.27';
-    const LATEST_PHP_5_4 = '5.4.20';
-    const LATEST_PHP_5_5 = '5.5.4';
+    const LATEST_PHP_5_4 = '5.4.21RC1';
+    const LATEST_PHP_5_5 = '5.5.5RC1';
 
     /**
      * Warning messages generated during loading of extensions references
