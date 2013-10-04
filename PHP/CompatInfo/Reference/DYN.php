b7c373333308de855053d5f1af7fe4a72c3aec7c
Laurent Laville <laurent.laville@gmail.com>
2013-10-04 10:38:16 +0200

    add auto-detection of ereg reference into DYN (lazy loader)

--- PHP/CompatInfo/Reference/DYN.php
+++ PHP/CompatInfo/Reference/DYN.php
@@ -119,6 +119,12 @@ class PHP_CompatInfo_Reference_DYN
                 'extension' => 'enchant',
             ),
             array(
+                'prefixes'  => array('ereg', 'split', 'sql_regcase'),
+                'suffixes'  => FALSE,
+                'contains'  => FALSE,
+                'extension' => 'ereg',
+            ),
+            array(
                 'prefixes'  => array('Mongo', 'bson_', 'MONGO'),
                 'suffixes'  => FALSE,
                 'contains'  => FALSE,
