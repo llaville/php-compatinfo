44df73a59ea818ca0089dc46ed1160e0cc8629cc
Laurent Laville <laurent.laville@gmail.com>
2013-11-19 11:53:30 +0100

    add new reference riak

---
+++ tests/Reference/RiakTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Unit tests for PHP_CompatInfo package, riak Reference
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    GIT: $Id$
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ * @since      Class available since Release 2.26.0
+ */
+
+require_once 'GenericTest.php';
+
+/**
+ * Tests for the PHP_CompatInfo class, retrieving components informations
+ * about riak extension
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ */
+class PHP_CompatInfo_Reference_RiakTest
+    extends PHP_CompatInfo_Reference_GenericTest
+{
+    /**
+     * Sets up the shared fixture.
+     *
+     * @covers PHP_CompatInfo_Reference_Riak::getClasses
+     * @covers PHP_CompatInfo_Reference_Riak::getFunctions
+     * @covers PHP_CompatInfo_Reference_Riak::getConstants
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$obj = new PHP_CompatInfo_Reference_Riak();
+        parent::setUpBeforeClass();
+    }
+}
