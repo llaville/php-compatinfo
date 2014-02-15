dd7e4c5fb690ba49f56b4ac23d35293a2ca5e413
Laurent Laville <laurent.laville@gmail.com>
2014-02-15 17:48:15 +0100

    This analyzer collects versions on all extensions of a project

---
+++ src/Bartlett/CompatInfo/Analyser/ExtensionAnalyser.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The CompatInfo Extension analyser accessible through the AnalyserPlugin.
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  GIT: $Id$
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ */
+
+namespace Bartlett\CompatInfo\Analyser;
+
+/**
+ * This analyzer collects versions on all extensions of a project.
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: @package_version@
+ * @link     http://php5.laurent-laville.org/compatinfo/
+ * @since    Class available since Release 3.0.0RC1
+ */
+class ExtensionAnalyser extends AbstractAnalyser
+{
+    const METRICS_PREFIX = 'ea';
+    const METRICS_GROUP  = 'extensions';
+
+    protected function init()
+    {
+        $this->count = array(
+            self::METRICS_PREFIX . '.' . self::METRICS_GROUP => array(),
+            self::METRICS_PREFIX . '.versions'               => array(
+                'ext.min' => '',
+                'ext.max' => '',
+                'php.min' => '4.0.0',
+                'php.max' => '',
+            )
+        );
+    }
+}
