30e27ad5085debc8a04da0aaffab0d22b89c9451
Laurent Laville <laurent.laville@gmail.com>
2014-03-17 20:21:01 +0100

    make the class loading compatible composer (source) and phar distribution

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,23 @@
 <?php
-require_once 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+$baseDir   = dirname(__DIR__);
+$vendorDir = $baseDir . '/vendor';
+
+require_once $vendorDir . '/nikic/php-parser/lib/bootstrap.php';
+require_once $vendorDir . '/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once $vendorDir . '/phpunit/php-timer/PHP/Timer.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Bartlett' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src',
-    'Symfony'  => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor',
-    'Bartlett\Tests\CompatInfo' => __DIR__,
+    'Symfony\\Component\\Finder'          => $vendorDir . '/symfony/finder',
+    'Symfony\\Component\\EventDispatcher' => $vendorDir . '/symfony/event-dispatcher',
+    'Symfony\\Component\\Console'         => $vendorDir . '/symfony/console',
+    'Symfony\\Component\\ClassLoader'     => $vendorDir . '/symfony/class-loader',
+    'Bartlett\\Reflect'                   => $vendorDir . '/bartlett/php-reflect/src',
+    'Bartlett\\Tests\\CompatInfo'         => $baseDir . '/tests',
+    'Bartlett'                            => $baseDir . '/src',
 ));
 $loader->register();
 
