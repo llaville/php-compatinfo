f99b5c941c051c1e56a50fad6fe363769fa5201a
Laurent Laville <laurent.laville@gmail.com>
2014-04-20 17:46:30 +0200

    migrate Shmop reference from 2.x to 3.x CompatInfo format

---
+++ src/Bartlett/CompatInfo/Reference/Extension/ShmopExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bartlett\CompatInfo\Reference\Extension;
+
+use Bartlett\CompatInfo\Reference\AbstractReference;
+
+class ShmopExtension extends AbstractReference
+{
+    const REF_NAME    = 'shmop';
+    const REF_VERSION = '';
+
+    public function __construct()
+    {
+        parent::__construct(self::REF_NAME, self::REF_VERSION);
+
+        $version  = $this->getCurrentVersion();
+        $releases = array();
+
+        // 4.0.4
+        if (version_compare($version, '4.0.4', 'ge')) {
+            $release = $this->getR40004();
+            $count = array_push($releases, $release);
+            $this->storage->attach($releases[--$count]);
+        }
+    }
+
+    protected function getR40004()
+    {
+        $release = new \StdClass;
+        $release->info = array(
+            'ext.min' => '4.0.4',
+            'ext.max' => '',
+            'state'   => 'stable',
+            'date'    => '2000-12-19',
+            'php.min' => '4.0.4',
+            'php.max' => '',
+        );
+        $release->functions = array(
+            'shmop_close'               => null,
+            'shmop_delete'              => null,
+            'shmop_open'                => null,
+            'shmop_read'                => null,
+            'shmop_size'                => null,
+            'shmop_write'               => null,
+        );
+        return $release;
+    }
+}
