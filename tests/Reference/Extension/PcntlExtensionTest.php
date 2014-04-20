501e1783dbf9d715d94e586156fb2a18b256408f
Laurent Laville <laurent.laville@gmail.com>
2014-04-20 09:54:23 +0200

    migrate Pcntl reference from 2.x to 3.x CompatInfo format

---
+++ tests/Reference/Extension/PcntlExtensionTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Unit tests for PHP_CompatInfo, pcntl extension Reference
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    GIT: $Id$
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ * @since      Class available since Release 3.0.0
+ */
+
+namespace Bartlett\Tests\CompatInfo\Reference\Extension;
+
+use Bartlett\Tests\CompatInfo\Reference\GenericTest;
+use Bartlett\CompatInfo\Reference\Extension\PcntlExtension;
+
+/**
+ * Tests for PHP_CompatInfo, retrieving components informations
+ * about pcntl extension
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ */
+class PcntlExtensionTest extends GenericTest
+{
+    /**
+     * Sets up the shared fixture.
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$optionalconstants = array(
+            'SI_NOINFO'
+        );
+
+        self::$obj = new PcntlExtension();
+        parent::setUpBeforeClass();
+    }
+}
