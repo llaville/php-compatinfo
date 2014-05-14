fed887cac923a937ef6de3b7cbd2e94df6c2ea6b
Laurent Laville <laurent.laville@gmail.com>
2014-05-14 19:21:47 +0200

    Searching for multiple location of compatinfo.json is now compatible both source code and phar version

--- phar-stub.php
+++ phar-stub.php
@@ -1,18 +1,18 @@
 #!/usr/bin/env php
 <?php
 if (class_exists('Phar')) {
-    Phar::mapPhar('phpreflect.phar');
+    Phar::mapPhar('phpcompatinfo.phar');
     Phar::interceptFileFuncs();
 
-    if (!getenv("REFLECT")) {
+    if (!getenv("COMPATINFO")) {
         $files = array(
-            realpath('./reflect.json'),
-            getenv('HOME').'/.config/phpreflect.json',
-            '/etc/phpreflect.json',
+            realpath('./compatinfo.json'),
+            getenv('HOME').'/.config/phpcompatinfo.json',
+            '/etc/phpcompatinfo.json',
         );
         foreach ($files as $file) {
             if (file_exists($file)) {
-                putenv("REFLECT=$file");
+                putenv("COMPATINFO=$file");
                 break;
             }
         }
