42f4915781f0b82f0b4bec42978024a8165601dd
Laurent Laville <laurent.laville@gmail.com>
2014-05-16 21:56:59 +0200

    fix broken unit test for SQLite3

--- src/Bartlett/CompatInfo/Reference/Extension/Sqlite3Extension.php
+++ src/Bartlett/CompatInfo/Reference/Extension/Sqlite3Extension.php
@@ -13,7 +13,7 @@ class Sqlite3Extension extends AbstractReference
     {
         parent::__construct(self::REF_NAME, self::REF_VERSION);
 
-        $version  = $this->getCurrentVersion();
+        $version  = $this->getLatestPhpVersion();
         $releases = array();
 
         // 5.3.0
