c395ce5e362f77b435b17b9ed686f568401a6d16
Laurent Laville <laurent.laville@gmail.com>
2014-06-12 16:52:01 +0200

    update APCu ref to 4.0.6

--- src/Bartlett/CompatInfo/Reference/Extension/ApcuExtension.php
+++ src/Bartlett/CompatInfo/Reference/Extension/ApcuExtension.php
@@ -6,7 +6,7 @@ use Bartlett\CompatInfo\Reference\AbstractReference;
 class ApcuExtension extends AbstractReference
 {
     const REF_NAME    = 'apcu';
-    const REF_VERSION = '4.0.5';    // 2014-06-11 (beta)
+    const REF_VERSION = '4.0.6';    // 2014-06-12 (beta)
 
     public function __construct()
     {
