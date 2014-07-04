fa84a0725bc4607202f186ebce013bd621dbd4eb
Laurent Laville <laurent.laville@gmail.com>
2014-07-04 19:03:27 +0200

    update latest PHP 5.6 version supported to RC2

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -8,7 +8,7 @@ abstract class AbstractReference implements ReferenceInterface
     const LATEST_PHP_5_3 = '5.3.28';
     const LATEST_PHP_5_4 = '5.4.30';
     const LATEST_PHP_5_5 = '5.5.14';
-    const LATEST_PHP_5_6 = '5.6.0RC1';
+    const LATEST_PHP_5_6 = '5.6.0RC2';
 
     protected $storage;
 
