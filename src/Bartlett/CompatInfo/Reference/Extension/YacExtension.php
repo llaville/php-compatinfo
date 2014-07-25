c63461af593e21af0b937521cae1b0610373fdb0
Remi Collet <fedora@famillecollet.com>
2014-07-25 10:52:34 +0200

    add Yac pecl extension in reference (version 0.9.1)
    Signed-off-by: Laurent Laville <laurent.laville@gmail.com>

---
+++ src/Bartlett/CompatInfo/Reference/Extension/YacExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Bartlett\CompatInfo\Reference\Extension;
+
+use Bartlett\CompatInfo\Reference\AbstractReference;
+
+class YacExtension extends AbstractReference
+{
+    const REF_NAME    = 'yac';
+    const REF_VERSION = '0.9.1';  // 2014-07-25 (beta)
+
+    public function __construct()
+    {
+        parent::__construct(self::REF_NAME, self::REF_VERSION);
+
+        $version  = $this->getCurrentVersion();
+        $releases = array();
+
+        // 0.9.0 - 2014-07-23 - first PECL release
+        if (version_compare($version, '0.9.0', 'ge')) {
+            $release = $this->getR00900();
+            $count = array_push($releases, $release);
+            $this->storage->attach($releases[--$count]);
+        }
+    }
+
+    protected function getR00900()
+    {
+        $release = new \StdClass;
+        $release->info = array(
+            'ext.min' => '0.9.0',
+            'ext.max' => '',
+            'state'   => 'beta',
+            'date'    => '2014-07-23',
+            'php.min' => '5.2.0',
+            'php.max' => '',
+        );
+        $release->iniEntries = array(
+            'yac.debug'                         => null,
+            'yac.enable'                        => null,
+            'yac.enable_cli'                    => null,
+            'yac.compress_threshold'            => null,
+            'yac.keys_memory_size'              => null,
+            'yac.values_memory_size'            => null,
+        );
+        $release->constants = array(
+            'YAC_VERSION'                       => null,
+            'YAC_MAX_KEY_LEN'                   => null,
+            'YAC_MAX_VALUE_RAW_LEN'             => null,
+            'YAC_MAX_RAW_COMPRESSED_LEN'        => null,
+            'YAC_SERIALIZER'                    => null,
+        );
+        $release->classes = array(
+            'Yac' => array(
+                'methods' => array(
+                    'add'                       => null,
+                    'set'                       => null,
+                    'get'                       => null,
+                    'delete'                    => null,
+                    'flush'                     => null,
+                    'info'                      => null,
+                    'dump'                      => null,
+                ),
+            ),
+        );
+        return $release;
+    }
+}
