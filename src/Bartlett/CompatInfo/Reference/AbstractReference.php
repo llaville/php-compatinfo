422c585936e98a71f15e9b685e23c59095582a70
Laurent Laville <laurent.laville@gmail.com>
2014-08-05 18:37:06 +0200

    add support for PHP 5.6.0RC3

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -8,7 +8,7 @@ abstract class AbstractReference implements ReferenceInterface
     const LATEST_PHP_5_3 = '5.3.28';
     const LATEST_PHP_5_4 = '5.4.31';
     const LATEST_PHP_5_5 = '5.5.15';
-    const LATEST_PHP_5_6 = '5.6.0RC2';
+    const LATEST_PHP_5_6 = '5.6.0RC3';
 
     protected $storage;
 
