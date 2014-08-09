866eba697ac9778bbcc5ff6161eb5cf695678f53
Laurent Laville <laurent.laville@gmail.com>
2014-08-09 11:04:49 +0200

    add support of new PHP 5.5 and 5.4 versions in RC1

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -6,8 +6,8 @@ abstract class AbstractReference implements ReferenceInterface
 {
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.29RC1';
-    const LATEST_PHP_5_4 = '5.4.31';
-    const LATEST_PHP_5_5 = '5.5.15';
+    const LATEST_PHP_5_4 = '5.4.32RC1';
+    const LATEST_PHP_5_5 = '5.5.16RC1';
     const LATEST_PHP_5_6 = '5.6.0RC3';
 
     protected $storage;
