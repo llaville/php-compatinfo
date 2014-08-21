fcb43649c3d1b0876ce6a772d646104c41f5f2bc
Laurent Laville <laurent.laville@gmail.com>
2014-08-21 20:35:40 +0200

    update PHP 5.5 and 5.4 stable versions

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -6,8 +6,8 @@ abstract class AbstractReference implements ReferenceInterface
 {
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.29';
-    const LATEST_PHP_5_4 = '5.4.32RC1';
-    const LATEST_PHP_5_5 = '5.5.16RC1';
+    const LATEST_PHP_5_4 = '5.4.32';
+    const LATEST_PHP_5_5 = '5.5.16';
     const LATEST_PHP_5_6 = '5.6.0RC4';
 
     protected $storage;
