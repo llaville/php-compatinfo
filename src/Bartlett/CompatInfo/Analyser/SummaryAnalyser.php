8fc4d613b0a2e9ce4f0f2191bdd8aba147099906
Laurent Laville <laurent.laville@gmail.com>
2014-08-21 13:53:38 +0200

    implement the GH-121 feature request

--- src/Bartlett/CompatInfo/Analyser/SummaryAnalyser.php
+++ src/Bartlett/CompatInfo/Analyser/SummaryAnalyser.php
@@ -12,9 +12,9 @@
 
 namespace Bartlett\CompatInfo\Analyser;
 
-use Bartlett\Reflect\Printer\Text;
-
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 /**
  * This analyzer collects versions on all elements of a project.
@@ -67,80 +67,105 @@ class SummaryAnalyser extends AbstractAnalyser
     public function render(OutputInterface $output)
     {
         $count = $this->count;
-        $lines = array();
+        $limit = function($elements, $title) {
+            uasort(
+                $elements,
+                function ($a, $b) {
+                    // sort by descending order
+                    return -1 * version_compare($a['php.min'], $b['php.min']);
+                }
+            );
+            list($ext, $values) = each($elements);
+            return array(
+                sprintf('<info>%s</info>', $title),
+                count($elements),
+                $values['php.min'],
+                $ext
+            );
+        };
 
-        $userFunctions = $internalFunctions = 0;
+        $output->writeln('<info>Global Analysis</info>' . PHP_EOL);
 
-        foreach ($count[self::METRICS_PREFIX . '.functions'] as $function) {
-            if ($function['ref'] == 'user') {
-                $userFunctions++;
-            } else {
-                $internalFunctions++;
+        $filter = func_get_arg(1);
+
+        if ($filter) {
+            $reports = array(
+                'Extension' => '.extensions',
+                'Namespace' => '.packages',
+                'Interface' => '.interfaces',
+                'Trait'     => '.traits',
+                'Class'     => '.classes',
+                'Function'  => '.functions',
+                'Constant'  => '.constants',
+            );
+            foreach ($reports as $title => $group) {
+                $this->listHelper(
+                    $output,
+                    $count[self::METRICS_PREFIX . $group],
+                    $count[self::METRICS_PREFIX . '.versions'],
+                    $filter,
+                    $title
+                );
+                $output->write(PHP_EOL);
             }
+        } else {
+            $userFunctions = $internalFunctions = array();
+
+            foreach ($count[self::METRICS_PREFIX . '.functions'] as $name => $function) {
+                if ($function['ref'] == 'user') {
+                    $userFunctions[$name] = $function;
+                } else {
+                    $internalFunctions[$name] = $function;
+                }
+            }
+
+            $headers = array('', 'Count', 'PHP min', 'Elements highlight');
+            $rows    = array();
+
+            $elements = $count[self::METRICS_PREFIX . '.extensions'];
+            $rows[] = $limit($elements, 'Extensions');
+
+            $elements = $count[self::METRICS_PREFIX . '.packages'];
+            $rows[] = $limit($elements, 'Namespaces');
+
+            $elements = $count[self::METRICS_PREFIX . '.interfaces'];
+            $rows[] = $limit($elements, 'Interfaces');
+
+            $elements = $count[self::METRICS_PREFIX . '.traits'];
+            $rows[] = $limit($elements, 'Traits');
+
+            $elements = $count[self::METRICS_PREFIX . '.classes'];
+            $rows[] = $limit($elements, 'Classes');
+
+            $elements = $userFunctions;
+            $rows[] = $limit($elements, 'User Functions');
+
+            $elements = $internalFunctions;
+            $rows[] = $limit($elements, 'Internal Functions');
+
+            $elements = $count[self::METRICS_PREFIX . '.constants'];
+            $rows[] = $limit($elements, 'Constants');
+
+            $footers = array(
+                '<info>Total</info>',
+                '',
+                sprintf(
+                    '<info>%s</info>',
+                    $count[self::METRICS_PREFIX . '.versions']['php.min']
+                ),
+                ''
+            );
+            $rows[] = new TableSeparator();
+            $rows[] = $footers;
+
+            $table = new Table($output);
+            $table
+                ->setHeaders($headers)
+                ->setRows($rows)
+                ->setStyle('compact')
+            ;
+            $table->render();
         }
-
-        $lines['reportTitle'] = array(
-            '%s<info>Summary Analysis</info>',
-            array(PHP_EOL)
-        );
-
-        $lines['summary'] = array(
-            '%sSummary',
-            array(PHP_EOL)
-        );
-        $lines['extensions'] = array(
-            '  Extensions                                %10d',
-            array(count($count[self::METRICS_PREFIX . '.extensions']))
-        );
-        $lines['namespaces'] = array(
-            '  Namespaces                                %10d',
-            array($count['namespaces'])
-        );
-        $lines['interfaces'] = array(
-            '  Interfaces                                %10d',
-            array(count($count[self::METRICS_PREFIX . '.interfaces']))
-        );
-        $lines['traits'] = array(
-            '  Traits                                    %10d',
-            array(count($count[self::METRICS_PREFIX . '.traits']))
-        );
-        $lines['classes'] = array(
-            '  Classes                                   %10d',
-            array(count($count[self::METRICS_PREFIX . '.classes']))
-        );
-        $lines['methods'] = array(
-            '  Methods                                   %10d',
-            array(count($count[self::METRICS_PREFIX . '.methods']))
-        );
-        $lines['functions'] = array(
-            '  Functions                                 %10d',
-            array($userFunctions)
-
-        );
-        $lines['constants'] = array(
-            '  Constants                                 %10d',
-            array(count($count[self::METRICS_PREFIX . '.constants']))
-        );
-        $lines['internalFunctions'] = array(
-            '  Internal Functions                        %10d',
-            array($internalFunctions)
-        );
-
-        $lines['versions'] = array(
-            '%sVersions',
-            array(PHP_EOL)
-        );
-        $lines['php.min'] = array(
-            '  PHP min                                   %10s',
-            array($count[self::METRICS_PREFIX . '.versions']['php.min'])
-        );
-        $lines['php.max'] = array(
-            '  PHP max                                   %10s',
-            array($count[self::METRICS_PREFIX . '.versions']['php.max'])
-        );
-
-        $printer = new Text;
-        $printer->write($output, $lines);
     }
 
     /**
