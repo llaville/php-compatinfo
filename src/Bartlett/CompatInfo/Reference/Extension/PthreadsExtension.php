658080c538aadfd775ef8c3a5959f3cb83b8db0f
Laurent Laville <laurent.laville@gmail.com>
2014-10-01 10:03:08 +0200

    update Pthreads ref to version 2.0.10 (stable)

--- src/Bartlett/CompatInfo/Reference/Extension/PthreadsExtension.php
+++ src/Bartlett/CompatInfo/Reference/Extension/PthreadsExtension.php
@@ -9,7 +9,7 @@ use Bartlett\CompatInfo\Reference\AbstractReference;
 class PthreadsExtension extends AbstractReference
 {
     const REF_NAME    = 'pthreads';
-    const REF_VERSION = '2.0.9';    // 2014-09-24 (stable)
+    const REF_VERSION = '2.0.10';    // 2014-10-01 (stable)
 
     public function __construct()
     {
