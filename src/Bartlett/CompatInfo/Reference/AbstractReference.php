f537b51e9ca93213a458a1f2ac8d3cc9a4cb0054
Laurent Laville <laurent.laville@gmail.com>
2014-10-16 15:29:24 +0200

    add support to new PHP stable versions 5.6.2, 5.5.18 and 5.4.34

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -6,9 +6,9 @@ abstract class AbstractReference implements ReferenceInterface
 {
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.29';
-    const LATEST_PHP_5_4 = '5.4.33';
-    const LATEST_PHP_5_5 = '5.5.18RC1';
-    const LATEST_PHP_5_6 = '5.6.1';
+    const LATEST_PHP_5_4 = '5.4.34';
+    const LATEST_PHP_5_5 = '5.5.18';
+    const LATEST_PHP_5_6 = '5.6.2';
 
     protected $storage;
 
