0acfb8b00ca2d820a88a65231ad5fe4cf2508e6b
Laurent Laville <laurent.laville@gmail.com>
2014-10-29 10:24:56 +0100

    end of refactoring validate command (fixed GH-136)

--- src/Bartlett/CompatInfo/Command/ValidateCommand.php
+++ src/Bartlett/CompatInfo/Command/ValidateCommand.php
@@ -14,13 +14,8 @@
 
 namespace Bartlett\CompatInfo\Command;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
-use Seld\JsonLint\JsonParser;
-use Seld\JsonLint\ParsingException;
+use Bartlett\CompatInfo\Environment;
+use Bartlett\Reflect\Command\AbstractValidateCommand;
 
 /**
  * Console command to validate structure of the JSON configuration file.
@@ -33,52 +28,8 @@ use Seld\JsonLint\ParsingException;
  * @link     http://php5.laurent-laville.org/compatinfo/
  * @since    Class available since Release 3.3.0
  */
-class ValidateCommand extends Command
+class ValidateCommand extends AbstractValidateCommand
 {
-    protected function configure()
-    {
-        $this
-            ->setName('validate')
-            ->setDescription('Validates a phpcompatinfo.json')
-            ->addArgument(
-                'file',
-                InputArgument::OPTIONAL,
-                'Path to phpcompatinfo.json file',
-                getenv("COMPATINFO") ? : './phpcompatinfo.json'
-            )
-        ;
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $file  = $input->getArgument('file');
-        $fname = realpath($file);
-
-        if (!file_exists($fname)) {
-            $output->writeln('<error>' . $file . ' not found.</error>');
-            return 1;
-        }
-        if (!is_readable($fname)) {
-            $output->writeln('<error>' . $file . ' is not readable.</error>');
-            return 1;
-        }
-
-        $json = file_get_contents($file);
-
-        try {
-            $parser = new JsonParser();
-            $parser->parse($json);
-        } catch (ParsingException $e) {
-            $fmt = $this->getApplication()->getHelperSet()->get('formatter');
-
-            $output->writeln(
-                $fmt->formatBlock(
-                    explode("\n", $e->getMessage()),
-                    'error'
-                )
-            );
-            return 1;
-        }
-        $output->writeln('<info>' . $file . ' is valid</info>');
-    }
+    protected $jsonFile = Environment::JSON_FILE;
+    protected $env      = Environment::ENV;
 }
