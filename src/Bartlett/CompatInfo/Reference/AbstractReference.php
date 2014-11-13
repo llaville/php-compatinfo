a88829c2f4ac8adf3fc62b5814ea8912433ef027
Laurent Laville <laurent.laville@gmail.com>
2014-11-13 14:22:30 +0100

    support new stable releases of branch 5.4/5.5/5.6

--- src/Bartlett/CompatInfo/Reference/AbstractReference.php
+++ src/Bartlett/CompatInfo/Reference/AbstractReference.php
@@ -6,9 +6,9 @@ abstract class AbstractReference implements ReferenceInterface
 {
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.29';
-    const LATEST_PHP_5_4 = '5.4.34';
-    const LATEST_PHP_5_5 = '5.5.19RC1';
-    const LATEST_PHP_5_6 = '5.6.3RC1';
+    const LATEST_PHP_5_4 = '5.4.35';
+    const LATEST_PHP_5_5 = '5.5.19';
+    const LATEST_PHP_5_6 = '5.6.3';
 
     protected $storage;
 
