d6f942dda5ff48ead8012ff1559967ca467ab50c
Laurent Laville <laurent.laville@gmail.com>
2015-01-06 18:05:39 +0100

    migrate regression test on bug GH-154

---
+++ tests/PhpFeaturesIssueTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Unit tests for PHP_CompatInfo package, issues reported
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    GIT: $Id$
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ * @since      Class available since Release 4.0.0-alpha2+1
+ */
+
+namespace Bartlett\Tests\CompatInfo\Reference;
+
+use Bartlett\Reflect\Client;
+
+/**
+ * Tests for PHP_CompatInfo, retrieving reference elements,
+ * and versioning information.
+ *
+ * @category   PHP
+ * @package    PHP_CompatInfo
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @author     Remi Collet <Remi@FamilleCollet.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://php5.laurent-laville.org/compatinfo/
+ */
+class PhpFeaturesIssueTest extends \PHPUnit_Framework_TestCase
+{
+    const GH154 = 'gh154.php';
+
+    protected static $fixtures;
+    protected static $api;
+
+    /**
+     * Sets up the shared fixture.
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$fixtures = __DIR__ . DIRECTORY_SEPARATOR
+            . 'fixtures' . DIRECTORY_SEPARATOR;
+
+        $client = new Client();
+
+        // request for a Bartlett\Reflect\Api\Analyser
+        self::$api = $client->api('analyser');
+    }
+
+    /**
+     * Regression test for feature GH#154
+     *
+     * @link https://github.com/llaville/php-compat-info/issues/154
+     *       Class member access on instantiation
+     * @link http://php.net/manual/en/migration54.new-features.php
+     * @group features
+     * @group regression
+     * @return void
+     */
+    public function testBugGH154()
+    {
+        $dataSource = self::$fixtures . self::GH154;
+        $analysers  = array('compatibility');
+        $metrics    = self::$api->run($dataSource, $analysers);
+        $versions   = $metrics['CompatibilityAnalyser']['versions'];
+
+        $this->assertEquals(
+            array(
+                'php.min'      => '5.4.0',
+                'php.max'      => '',
+            ),
+            $versions
+        );
+    }
+}
