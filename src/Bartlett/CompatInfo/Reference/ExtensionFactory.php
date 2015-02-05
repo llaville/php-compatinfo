37b9089c81188d7071c2638e9254db336c3ff6b0
Laurent Laville <laurent.laville@gmail.com>
2015-02-05 19:29:10 +0100

    add support of PHP 5.6.6RC1

--- src/Bartlett/CompatInfo/Reference/ExtensionFactory.php
+++ src/Bartlett/CompatInfo/Reference/ExtensionFactory.php
@@ -30,7 +30,7 @@ class ExtensionFactory implements ReferenceInterface
     const LATEST_PHP_5_3 = '5.3.29';
     const LATEST_PHP_5_4 = '5.4.37';
     const LATEST_PHP_5_5 = '5.5.21';
-    const LATEST_PHP_5_6 = '5.6.5';
+    const LATEST_PHP_5_6 = '5.6.6RC1';
 
     protected $storage;
 
