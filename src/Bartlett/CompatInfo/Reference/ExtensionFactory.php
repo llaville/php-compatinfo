818ae79e06217001ab1b1bcc9cab8e48b8c45bee
Laurent Laville <laurent.laville@gmail.com>
2015-02-19 20:17:35 +0100

    add support of new PHP versions 5.4.38 and 5.5.22

--- src/Bartlett/CompatInfo/Reference/ExtensionFactory.php
+++ src/Bartlett/CompatInfo/Reference/ExtensionFactory.php
@@ -28,8 +28,8 @@ class ExtensionFactory implements ReferenceInterface
 {
     const LATEST_PHP_5_2 = '5.2.17';
     const LATEST_PHP_5_3 = '5.3.29';
-    const LATEST_PHP_5_4 = '5.4.37';
-    const LATEST_PHP_5_5 = '5.5.22RC1';
+    const LATEST_PHP_5_4 = '5.4.38';
+    const LATEST_PHP_5_5 = '5.5.22';
     const LATEST_PHP_5_6 = '5.6.6RC1';
 
     protected $storage;
