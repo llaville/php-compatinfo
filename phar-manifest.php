45c2e01bbdc0d62e88eeb64472d0649110be5397
Laurent Laville <laurent.laville@gmail.com>
2015-03-28 16:47:58 +0100

    Remove Error Control Operator

--- phar-manifest.php
+++ phar-manifest.php
@@ -5,13 +5,13 @@
  */
 print 'bartlett/php-compatinfo: <info>';
 
-$tag = @exec('git describe --tags 2>&1');
+$tag = exec('git describe --tags 2>&1');
 
 if (strpos($tag, '-') === false && strpos($tag, 'No names found') === false) {
     print $tag;
 } else {
-    $branch = @exec('git rev-parse --abbrev-ref HEAD');
-    $hash   = @exec('git log -1 --format="%H"');
+    $branch = exec('git rev-parse --abbrev-ref HEAD');
+    $hash   = exec('git log -1 --format="%H"');
     print $branch . '@' . $hash;
 }
 
