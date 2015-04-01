604e102283341cc4f26922afe148f5cc8407a0d3
Remi Collet <fedora@famillecollet.com>
2015-04-01 12:25:52 +0200

    fix typo

--- tests/Reference/GenericTest.php
+++ tests/Reference/GenericTest.php
@@ -630,7 +630,7 @@ class GenericTest extends \PHPUnit_Framework_TestCase
             }
 
             foreach ($constants as $constantname => $constantvalue) {
-                if (in_array("$classname::$constname", self::$optionalconstants)) {
+                if (in_array("$classname::$constantname", self::$optionalconstants)) {
                     continue;
                 }
                 $this->assertArrayHasKey(
