f3032e11ac9152941ecfc001d21a4d0ecf736445
Laurent Laville <laurent.laville@gmail.com>
2015-11-21 10:12:27 +0100

    added optional MySQLi constants missing

--- tests/Reference/Extension/MysqliExtensionTest.php
+++ tests/Reference/Extension/MysqliExtensionTest.php
@@ -47,7 +47,11 @@ class MysqliExtensionTest extends GenericTest
             'MYSQLI_OPT_NET_READ_BUFFER_SIZE',
             'MYSQLI_OPT_INT_AND_FLOAT_NATIVE',
             'MYSQLI_ASYNC',
-            'MYSQLI_ON_UPDATE_NOW_FLAG',
+            'MYSQLI_STORE_RESULT_COPY_DATA',
+
+            // Requires DBUG_ON
+            'MYSQLI_DEBUG_TRACE_ENABLED',
+
             // Requires REFRESH_BACKUP_LOG
             'MYSQLI_REFRESH_BACKUP_LOG',
             // Requires SERVER_QUERY_WAS_SLOW
@@ -57,6 +61,41 @@ class MysqliExtensionTest extends GenericTest
             // requires MYSQL_VERSION_ID >= 50611 or MYSQLI_USE_MYSQLND
             'MYSQLI_OPT_CAN_HANDLE_EXPIRED_PASSWORDS',
             'MYSQLI_CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS',
+
+            // Requires FIELD_TYPE_JSON
+            'MYSQLI_TYPE_JSON',
+
+            // Requires MYSQL_DATA_TRUNCATED
+            'MYSQLI_DATA_TRUNCATED',
+
+            // Requires MYSQLI_USE_MYSQLND
+            // and
+            // Requires MYSQL > 50111
+            'MYSQLI_OPT_SSL_VERIFY_SERVER_CERT',
+
+            // Requires MYSQL > 50002
+            'MYSQLI_TYPE_NEWDECIMAL',
+            'MYSQLI_TYPE_BIT',
+
+            // Requires MYSQL > 50605
+            'MYSQLI_SERVER_PUBLIC_KEY',
+
+            // Requires MYSQL > 50003
+            'MYSQLI_STMT_ATTR_CURSOR_TYPE',
+            'MYSQLI_CURSOR_TYPE_NO_CURSOR',
+            'MYSQLI_CURSOR_TYPE_READ_ONLY',
+            'MYSQLI_CURSOR_TYPE_FOR_UPDATE',
+            'MYSQLI_CURSOR_TYPE_SCROLLABLE',
+
+            // Requires MYSQL > 50007
+            'MYSQLI_STMT_ATTR_PREFETCH_ROWS',
+
+            // Requires MYSQL > 50007
+            'MYSQLI_NO_DEFAULT_VALUE_FLAG',
+
+            // Requires MYSQL > 51122 and < 60000 || > 60003
+            'MYSQLI_ON_UPDATE_NOW_FLAG'
+
         );
         self::$optionalfunctions = array(
             // Requires HAVE_EMBEDDED_MYSQLI
