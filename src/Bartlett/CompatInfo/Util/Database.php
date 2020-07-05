9e2efc6bbcf36eed0837441a9d8141e98cf5d67f
Laurent Laville <laurent.laville@gmail.com>
2020-07-05 08:19:51 +0000

    phpstan help up to find code to be removed

--- src/Bartlett/CompatInfo/Util/Database.php
+++ src/Bartlett/CompatInfo/Util/Database.php
@@ -3,45 +3,29 @@
 namespace Bartlett\CompatInfo\Util;
 
 use Bartlett\CompatInfoDb\DatabaseFactory;
-use Bartlett\CompatInfoDb\Environment;
 use PDO;
 
 /**
  * Class Database
  * Keep compatibility between PHP_CompatInfo_Db 1.x (deprecated) and 2.x
+ * Since version 5.3.0, only compatibility to PHP_CompatInfo_Db 2.x is supported
  */
 class Database
 {
     public static function versionRefDb()
     {
-        if (class_exists('Bartlett\\CompatInfoDb\\Environment')) {
-            // CompatInfo DB 1.x
-            $versions = Environment::versionRefDb();
-        } else {
-            // CompatInfo DB 2.x
-            $pdo = DatabaseFactory::create('sqlite');
+        $pdo = DatabaseFactory::create('sqlite');
 
-            $stmt = $pdo->prepare(
-                'SELECT build_string as "build.string", build_date as "build.date", build_version as "build.version"' .
-                ' FROM bartlett_compatinfo_versions'
-            );
-            $stmt->execute();
-            $versions = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-
-        return $versions;
+        $stmt = $pdo->prepare(
+            'SELECT build_string as "build.string", build_date as "build.date", build_version as "build.version"' .
+            ' FROM bartlett_compatinfo_versions'
+        );
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public static function initRefDb()
     {
-        if (class_exists('Bartlett\\CompatInfoDb\\Environment')) {
-            // CompatInfo DB 1.x
-            $pdo = Environment::initRefDb();
-        } else {
-            // CompatInfo DB 2.x
-            $pdo = DatabaseFactory::create('sqlite');
-        }
-
-        return $pdo;
+        return DatabaseFactory::create('sqlite');
     }
 }
