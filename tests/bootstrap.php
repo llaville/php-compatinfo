92c1565cad5276f0dba486d57093f667aac372a8
Laurent Laville <laurent.laville@gmail.com>
2021-03-12 06:19:01 +0000

    raise bartlett/php-compatinfo-db to minimum 3.4 to allow Autogenerate Doctrine Proxy files (see APP_PROXY_DIR env var)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,8 @@
 <?php
+
+use Bartlett\CompatInfoDb\Presentation\Console\ApplicationInterface;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+putenv('APP_ENV=' . ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod'));
+putenv('APP_PROXY_DIR=' . ($_SERVER['APP_PROXY_DIR'] ?? $_ENV['APP_PROXY_DIR'] ?? '/tmp/bartlett/php-compatinfo-db/' . ApplicationInterface::VERSION . '/proxies'));
