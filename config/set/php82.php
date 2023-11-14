5f6df1fe781f17461f348bf18e611367165a4f47
Laurent Laville <laurent.laville@gmail.com>
2023-11-14 09:24:36 +0000

    fix additional services into container about PHP 8.2 sniffs

--- config/set/php82.php
+++ config/set/php82.php
@@ -6,6 +6,12 @@
  * file that was distributed with this source code.
  */
 
+use Bartlett\CompatInfo\Application\Sniffs\Attributes\AllowDynamicPropertiesAttributeSniff;
+use Bartlett\CompatInfo\Application\Sniffs\Attributes\SensitiveParameterAttributeSniff;
+use Bartlett\CompatInfo\Application\Sniffs\Classes\ReadonlyClassSniff;
+use Bartlett\CompatInfo\Application\Sniffs\Constants\ConstantsInTraitsSniff;
+use Bartlett\CompatInfo\Application\Sniffs\TextProcessing\DeprecateDollarBraceStringInterpolationSniff;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 /**
@@ -15,4 +21,15 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
  * @since Release 6.5.0
  */
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->autowire()
+    ;
+
+    $services->set(AllowDynamicPropertiesAttributeSniff::class);
+    $services->set(SensitiveParameterAttributeSniff::class);
+    $services->set(ReadonlyClassSniff::class);
+    $services->set(ConstantsInTraitsSniff::class);
+    $services->set(DeprecateDollarBraceStringInterpolationSniff::class);
 };
