4ff416f8691aef428581410abc254338d8ec0752
Laurent Laville <laurent.laville@gmail.com>
2024-02-08 07:29:49 +0000

    be able to convert to GitHub Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 [ "$APP_DEBUG" == 'true' ] && set -x
 set -e
 
+composer_global_home="/home/$(id -u -n)/.composer"
+
 if [ "$APP_DEBUG" == 'true' ]
 then
     echo "> You will act as user: $(id -u -n)"
-    echo "$(composer config --global --list)"
-    /bin/sh -c "ls -l $(composer config --global home)"
+    echo "> Path to Composer home dir: ${composer_global_home}"
 fi
 
-php -d memory_limit=512M "$(composer config --global home)/vendor/bin/phpcompatinfo" $@
+php -d memory_limit=512M "${composer_global_home}/vendor/bin/phpcompatinfo" $@
