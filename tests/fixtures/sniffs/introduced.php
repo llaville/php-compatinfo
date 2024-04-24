b03371c4248634ce2bd81c86b178f7078bc6edfb
Viktor Szépe <viktor@szepe.net>
2024-04-24 07:18:00 +0200

    Revert introduced.php

--- tests/fixtures/sniffs/introduced.php
+++ tests/fixtures/sniffs/introduced.php
@@ -8,5 +8,5 @@ class SplTempFileObject {}
 
 print_r(ini_get_all("pcre", true));
 
-$a = array('<foo>', "'bar'", '"baz"', '&blong&', "\xc3\xa9");
+$a = array('<foo>',"'bar'",'"baz"','&blong&', "\xc3\xa9");
 $json = json_encode($a, JSON_PRETTY_PRINT, 256);
