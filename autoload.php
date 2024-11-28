f54bacf66f25e63edb7cef6acf6def0ba8f966f7
Laurent Laville <laurent.laville@gmail.com>
2024-11-28 05:49:13 +0000

    bootstrap file used now the new Application autoloader

---
+++ autoload.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the PHP_CompatInfo package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Laurent Laville
+ * @since Release 7.2.0
+ */
+namespace Bartlett\CompatInfo;
+
+use RuntimeException;
+use function basename;
+use function dirname;
+use function file_exists;
+use function implode;
+use function spl_autoload_register;
+use function sprintf;
+use const DIRECTORY_SEPARATOR;
+
+if (class_exists(__NAMESPACE__ . '\Autoload', false) === false) {
+    class Autoload
+    {
+        /**
+         * The composer autoloader.
+         *
+         * @var \Composer\Autoload\ClassLoader
+         */
+        private static $composerAutoloader = null;
+
+        public static function load(string $class): void
+        {
+            if (self::$composerAutoloader === null) {
+                self::$composerAutoloader = require self::getAutoloadFile();
+            }
+
+            self::$composerAutoloader->loadClass($class);
+        }
+
+        private static function getAutoloadFile(): string
+        {
+            if (isset($GLOBALS['_composer_autoload_path'])) {
+                $possibleAutoloadPaths = [
+                    dirname($GLOBALS['_composer_autoload_path'])
+                ];
+                $autoloader = basename($GLOBALS['_composer_autoload_path']);
+            } else {
+                $possibleAutoloadPaths = [
+                    // local dev repository
+                    __DIR__,
+                    // dependency
+                    dirname(__DIR__, 3),
+                ];
+                $autoloader = 'vendor/autoload.php';
+            }
+
+            foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+                if (file_exists($possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader)) {
+                    return $possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader;
+                }
+            }
+
+            throw new RuntimeException(
+                sprintf(
+                    'Unable to find "%s" in "%s" paths.',
+                    $autoloader,
+                    implode('", "', $possibleAutoloadPaths)
+                )
+            );
+        }
+    }
+
+    spl_autoload_register(__NAMESPACE__ . '\Autoload::load', true, true);
+}
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,37 +8,4 @@
  * @author Laurent Laville
  */
 
-if (isset($_composer_autoload_path)) {
-    $possibleAutoloadPaths = [
-        dirname($_composer_autoload_path)
-    ];
-    $autoloader = basename($_composer_autoload_path);
-} else {
-    $possibleAutoloadPaths = [
-        // local dev repository
-        dirname(__DIR__),
-        // dependency
-        dirname(__DIR__, 4),
-    ];
-    $autoloader = 'vendor/autoload.php';
-}
-
-$isAutoloadFound = false;
-foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
-    if (file_exists($possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader)) {
-        require_once $possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader;
-        $isAutoloadFound = true;
-        $vendorDir = $possibleAutoloadPath . DIRECTORY_SEPARATOR . dirname($autoloader);
-        break;
-    }
-}
-
-if ($isAutoloadFound === false) {
-    fwrite(
-        STDERR,
-        'You need to set up the project dependencies using Composer:' . PHP_EOL . PHP_EOL .
-        '    composer install' . PHP_EOL . PHP_EOL .
-        'You can learn all about Composer on https://getcomposer.org/.' . PHP_EOL
-    );
-    exit(1);
-}
+require_once dirname(__DIR__) . '/autoload.php';
